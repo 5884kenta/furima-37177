eda822266a75b97caf12afd203c376040e676883
5884kenta <babolat.i.kamo@gmail.com>
2021-12-16 18:18:54 +0900

    出品→データベース登録

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,2 +1,13 @@
-let addTaxPrice = document.getElementById("add-tax-price")
-console.log(addTaxPrice)
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue / 10)
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(inputValue - inputValue / 10)
+    
+  })
+});
+ 
+ 
