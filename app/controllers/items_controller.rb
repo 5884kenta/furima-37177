5f440add97af6264bede47f85db8d84d14f19ed9
5884kenta <babolat.i.kamo@gmail.com>
2021-12-17 17:21:36 +0900

    rubocop　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, ]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -27,9 +27,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-
     if @item.update(item_params)
-       redirect_to item_path(@item.id)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
@@ -49,6 +48,4 @@ class ItemsController < ApplicationController
   def contributor_confirmation
     redirect_to root_path unless current_user == @item.user
   end
-
-
 end
